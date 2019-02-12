166a446342da3024fe20504a5d2dcd6be8a50dd0
damien arnaud <darnaud@mail.com>
2019-02-12 16:21:36 +0100

    2 done

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,36 @@
+class TasksController < ApplicationController
+  # As a user, I can list tasks
+  def index
+    @tasks = Task.all
+  end
+  # As a user, I can view the details of a task
+  def show
+    @task = Task.find(params[:id])
+  end
+  # As a user, I can add a new task
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+  # As a user, I can edit a task (mark as completed / update title & details)
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task.id)
+  end
+  # As a user, I can remove a task
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
