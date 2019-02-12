c2ea1b7fd5b295396d468b08d26821b9741024ce
damien arnaud <darnaud@mail.com>
2019-02-12 17:03:42 +0100

    refacto 2nd step

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   # As a user, I can list tasks
   def index
     @tasks = Task.all
   end
   # As a user, I can view the details of a task
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
   # As a user, I can add a new task
   def new
@@ -19,17 +20,17 @@ class TasksController < ApplicationController
   end
   # As a user, I can edit a task (mark as completed / update title & details)
   def edit
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task.id)
   end
   # As a user, I can remove a task
   def destroy
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
   end
